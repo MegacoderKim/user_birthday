ada3e7a38ba35abead2696d8fda03f4d52ea97cd
Harrison Kimani <harrikimm@outlook.com>
2021-01-25 17:26:02 +0300

    Browsable url

--- user_birthday/urls.py
+++ user_birthday/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-
+from rest_framework.documentation import include_docs_urls
 
 apipatterns = [path("", include("birthday.urls"))]
 
-urlpatterns = [path("api/v1", include(apipatterns))]
+urlpatterns = [
+    path("", include_docs_urls(title="Birthdays API")),
+    path("api/v1", include(apipatterns)),
+]
